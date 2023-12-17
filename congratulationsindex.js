d99f9b1990dca46e051243173e8582e83d23bebc
TRajaShekhar <116106766+TRajaShekhar@users.noreply.github.com>
2023-12-17 07:09:43 +0530

    Create congratulationsindex.js

---
+++ congratulationsindex.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div className="head-container">
+    <h1>Congratulations </h1>
+    <div className="card">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"></img>
+      <h1 className="name">Thumpati Raja sekhar</h1>
+      <p>Vishnu Institute of Computer education and Technology Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
